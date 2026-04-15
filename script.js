9c24f3df903ee526e54b1b07d2ceece10627f456
Anna <annamartins08@gmail.com>
2026-04-15 10:11:56 -0300

    corrijindo erro e finalizando exercicio 1 no script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let numero = Number(prompt("digite um número inteiro."))
 
-if(isNaNn(umero)) {
+if(isNaNn(numero)) {
     alert("o que você digitou não é um número");
 }else { 
     if(numero %2==0) {
