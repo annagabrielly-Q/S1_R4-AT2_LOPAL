c65e767c0d11ae92f689adc1695c5a69824f1197
Anna <annamartins08@gmail.com>
2026-04-15 10:09:47 -0300

    finalizando exercicio 1 no script

---
+++ script.js
@@ -0,0 +1,11 @@
+let numero = Number(prompt("digite um número inteiro."))
+
+if(isNaN{numero}) {
+    alert("o que você digitou não é um número");
+}else { 
+    if(numero %2==0) {
+        alert ("É um número par");
+    }else {
+        alert("É um número impar");
+    }
+}
