b29707068f450c1fe2160bd4bf70409d11b903ca
Anna <annamartins08@gmail.com>
2026-05-06 10:57:27 -0300

    finalizando script de soma

--- desafio/S1_R5-AT1_LOPAL/script.js
+++ desafio/S1_R5-AT1_LOPAL/script.js
@@ -1,7 +1,7 @@
-let mensagem = "";
+let soma =0
+for (let i =1; i <=10;i++){
+   soma += i;
 
-for (let i = 10; i >= 1; i--) {
- //mensagem "10,9,8,7,6,5,4,3,2,1"
-   mensagem = `${mensagem} ${i}`;
 }
-alert (mensagem);
+   
+alert (`a soma total é: ${soma}`)
