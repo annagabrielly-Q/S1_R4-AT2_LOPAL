ee70f8505580dafef0e7489a5e8ea25e1397f46c
Anna <annamartins08@gmail.com>
2026-05-06 09:35:42 -0300

    fix: corrigida a lógica de looping for

--- desafio/S1_R5-AT1_LOPAL/script.js
+++ desafio/S1_R5-AT1_LOPAL/script.js
@@ -1,8 +1,7 @@
-let mensagem = "10,9,8,7,6,5,4,3,2,1";
+let mensagem = "";
 
-for (let i = 10; i < 1; i--) {
-
-   //mensagem "10,9,8,7,6,5,4,3,2,1"
+for (let i = 10; i >= 1; i--) {
+ //mensagem "10,9,8,7,6,5,4,3,2,1"
    mensagem = `${mensagem} ${i}`;
 }
 alert (mensagem);
